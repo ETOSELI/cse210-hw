1032754dd45c348e75556e8f55c962a6d9e7e5f2
etoseli <toselie3@gmail.com>
2024-06-29 15:12:46 +0300

    wrote a script memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,119 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
+// Reference class
+public class Reference
+{
+    public string Book { get; private set; }
+    public int Chapter { get; private set; }
+    public int VerseStart { get; private set; }
+    public int VerseEnd { get; private set; }
+
+    // Constructor for a single verse
+    public Reference(string book, int chapter, int verse)
+    {
+        Book = book;
+        Chapter = chapter;
+        VerseStart = verse;
+        VerseEnd = verse;
+    }
+
+    // Constructor for a verse range
+    public Reference(string book, int chapter, int verseStart, int verseEnd)
+    {
+        Book = book;
+        Chapter = chapter;
+        VerseStart = verseStart;
+        VerseEnd = verseEnd;
+    }
+
+    public string GetDisplayText()
+    {
+        if (VerseStart == VerseEnd)
+            return $"{Book} {Chapter}:{VerseStart}";
+        else
+            return $"{Book} {Chapter}:{VerseStart}-{VerseEnd}";
+    }
+}
+
+// Word class
+public class Word
+{
+    public string Text { get; private set; }
+    public bool IsHidden { get; private set; }
+
+    public Word(string text)
+    {
+        Text = text;
+        IsHidden = false;
+    }
+
+    public void Hide()
+    {
+        IsHidden = true;
+    }
+
+    public string GetDisplayText()
+    {
+        return IsHidden ? new string('_', Text.Length) : Text;
+    }
+}
+
+// Scripture class
+public class Scripture
+{
+    public Reference Reference { get; private set; }
+    private List<Word> Words;
+
+    public Scripture(Reference reference, string text)
+    {
+        Reference = reference;
+        Words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords(int count)
+    {
+        Random random = new Random();
+        int hiddenCount = 0;
+        while (hiddenCount < count)
+        {
+            int index = random.Next(Words.Count);
+            if (!Words[index].IsHidden)
+            {
+                Words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        var wordsDisplay = Words.Select(word => word.GetDisplayText()).ToList();
+        return $"{Reference.GetDisplayText()} {string.Join(" ", wordsDisplay)}";
+    }
+}
+
+// Program class
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        var reference = new Reference("Proverbs", 3, 5, 6);
+        var scripture = new Scripture(reference, "Trust in the LORD with all your heart and lean not on your own understanding");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit.");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
     }
 }
